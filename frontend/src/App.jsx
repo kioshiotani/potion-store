f8c595462385b810832c01c0e7b2e068a09993c7
kioshiotani <brunokioshiotani@gmail.com>
2026-06-17 16:41:17 -0300

    chore: organização do repositorio
    iniciado frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+
+// testes
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import styles from './Header.module.css'
+import { GiPotionBall } from "react-icons/gi";
+
+
+export default function Header() {
+    return (
+        <>
+        <div className="container-fluid p-0">
+        <header className={`w-100 d-flex justify-content-start align-items-center py-3 border-bottom border-dark gap-4`}> 
+            <a className={`d-flex align-items-center gap-4 mx-2 text-decoration-none fs-4 ${styles.logo}`} href="#">
+                <GiPotionBall className={`${styles.potion} h1`}/>
+                <span className="text-alert">Poções e Soluções</span>
+            </a>
+            <ul className={`nav nav-pills ${styles.link}`}> 
+                <li className="nav-item"><a href="#" className={`nav-link ${styles.link}`}>História</a></li> 
+                <li className="nav-item"><a href="#" className={`nav-link ${styles.link}`}>Produtos</a></li> 
+                <li className="nav-item"><a href="#" className={`nav-link ${styles.link}`}>Contato</a></li> 
+                <li className="nav-item"><a href="#" className={`nav-link ${styles.link}`}>Sobre</a></li> 
+            </ul>
+        </header>
+        </div>
+        </>
+    )
+}
